f7c78b89924e10981cbbdb4d99cbeacf06b53a0e
Beryl1808 <dkwafo17@gmail.com>
2024-12-01 19:40:28 +0000

    1commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Print a sentence
+print("Such a lovely Day \n")
+#Print a number
+print(2024)
+#Print text with number
+print("Lovely luxe", 10)
+#End arguement print of statement
+print("Goodbye", end= "$")
