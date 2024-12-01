6a1158e08973b8e5bda2cb68677302f80f01de2e
Beryl1808 <dkwafo17@gmail.com>
2024-12-01 19:59:53 +0000

    2 cm

---
+++ Variables.py
@@ -0,0 +1,12 @@
+#Creating different variables
+x= 4
+y= "Alex"
+print(x)
+print(y)
+
+codingal="codingal"
+
+#User Input
+name = input("enter your name:")
+
+print("\n hello", name, "\n Welcome to codingal")
